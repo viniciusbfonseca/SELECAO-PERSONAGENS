14660b90e7c03256a88271837b38c5855e7499a0
Vinicius Fonseca <vinicius.borges95@hotmail.com>
2025-04-04 18:42:27 -0300

    adicionando anotações e dicas no codigo js

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,24 +1,28 @@
-/*
-OBJETIVO 1 - QUANDO PASSAR O MOUSE EM CIMA DO PERSONAGEM TEMOS QUE:
 
 
-- COLOCAR A CLASSE SELECIONADO NO PERSONAGEM QUE POSSAMOS O MOUSE EM CIMA PARA ADICIONAR ANIMAÇÃO NELE
-
-- RETIRAR A CLASSE SELECIONADO DO PERSONAGEM QUE ESTÁ SELECIONADO 
-
-
-OBJETIVO 2 -  QUANDO PASSAR O MOUSE EM CIMA DO PERSONAGEM, TROCAR A IMAGEM E NOME DO PERSONAGEM GRANDE 
-
-- Alterar a imagem do jogador 1
-
-- Alterar o nome do jogador 1 
-
-*/ 
 
 const personagens = document.querySelectorAll('.personagem');
 
-personagens.forEach((personagem) => { 
+personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
-        personagem.classList.add('selecionado');
-    })
-})
+
+        // OBJETIVO 1 - QUANDO PASSAR O MOUSE EM CIMA DO PERSONAGEM TEMOS QUE:
+
+        // - COLOCAR A CLASSE SELECIONADO NO PERSONAGEM QUE POSSAMOS O MOUSE EM CIMA PARA ADICIONAR ANIMAÇÃO NELE
+
+        // - RETIRAR A CLASSE SELECIONADO DO PERSONAGEM QUE ESTÁ SELECIONADO 
+        const personagemSelecionado = document.querySelector('.selecionado')
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado')
+
+        // OBJETIVO 2 -  QUANDO PASSAR O MOUSE EM CIMA DO PERSONAGEM, TROCAR A IMAGEM E NOME DO PERSONAGEM GRANDE 
+
+        // - Alterar a imagem do jogador 1
+
+        //  Alterar o nome do jogador 1 
+
+    });
+});
+
+
